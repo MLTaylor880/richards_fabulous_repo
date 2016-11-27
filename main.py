9db0f26efb553e1b246bb346600dc77d24afb5de
Richard Harris <richard.jonathon.harris@gmail.com>
2016-11-27 13:00:20 -0600

    Hello world and the story of pets

---
+++ main.py
@@ -0,0 +1,20 @@
+from pets.pets import Pet
+
+if __name__ == '__main__':
+    print 'This is the story of my pet!'
+    spritz = Pet('Spritz', 'cat')
+    print 'My pet is %s, a very happy %s' % (spritz.name, spritz.type)
+    print 'Let\'s see how he is doing!'
+    spritz.wants_to_play()
+    spritz.wants_to_eat()
+    print 'Oh no! We should feed and play with %s!' % spritz.name
+    amount_to_feed = raw_input('How many times do you want to feed %s? ' % spritz.name)
+    for x in range(0, int(amount_to_feed)):
+        spritz.feed()
+    amount_to_play = raw_input('How many times do you want to play with %s? ' % spritz.name)
+    for x in range(0, int(amount_to_play)):
+        spritz.play()
+    print 'How is %s doing now?' % spritz.name
+    spritz.wants_to_play()
+    spritz.wants_to_eat()
+    print 'Bye %s, you have been an awesome %s!' % (spritz.name, spritz.type)
---
+++ pets/pets.py
@@ -0,0 +1,33 @@
+class Pet(object):
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.hunger = 0
+        self.boredom = 10
+
+    def feed(self):
+        if self.hunger < 5:
+            print '%s eats some food!' % self.name
+            self.hunger += 1
+        else:
+            print '%s knocks your food on the ground.' % self.name
+
+    def play(self):
+        if self.boredom > 6:
+            print '%s runs around like a maniac!' % self.name
+            self.boredom -= 1
+        else:
+            print '%s just stares at you.' % self.name
+
+    def wants_to_play(self):
+        if self.boredom >= 6:
+            print 'Your pet %s is really bored! You should play with it more!' % self.name
+        else:
+            print 'Your pet %s just wants to sleep. Leave it alone!' % self.name
+
+    def wants_to_eat(self):
+        if self.hunger < 3:
+            print 'Your pet %s is pretty hungry. You should feed it!' % self.name
+        else:
+            print 'Your pet %s doesn\'t need to eat right now.' % self.name
